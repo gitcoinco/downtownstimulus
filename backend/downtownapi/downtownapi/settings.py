90f674b4d6219055a5d2e4f6a8c27d299ed511a3
Manank Patni <manank321@gmail.com>
2020-06-01 21:11:15 +0530

    Add Allowed Hosts

--- backend/downtownapi/downtownapi/settings.py
+++ backend/downtownapi/downtownapi/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'hlp5)_$^uyul5aqmd!yngg)jlf$j-#utopl3avrwrtuj4+=mhl'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*', 'localhost', '127.0.0.1', '*']
 
 
 # Application definition
