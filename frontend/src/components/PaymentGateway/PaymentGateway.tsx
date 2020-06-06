8c01e804f8a86ad4e14bd2692baa07f7caa47fdf
Sanchay Mittal <sanchaymittal@gmail.com>
2020-06-06 22:16:32 +0530

    new: payment gateway new: clr basic example setup

---
+++ frontend/src/components/PaymentGateway/PaymentGateway.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { loadStripe } from '@stripe/stripe-js';
+import {
+    CardElement,
+    Elements,
+    useStripe,
+    useElements,
+} from '@stripe/react-stripe-js';
+
+const CheckoutForm = () => {
+    const stripe = useStripe();
+    const elements = useElements();
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const { error, paymentMethod } = await stripe.createPaymentMethod({
+            type: 'card',
+            card: elements.getElement(CardElement),
+        });
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <CardElement />
+            <button type="submit" disabled={!stripe}>
+                Pay
+        </button>
+        </form>
+    );
+};
+const stripePromise = loadStripe('pk_test_6pRNASCoBOKtIshFeQd4XMUh');
+
+const PaymentGateway = () => (
+    <Elements stripe={stripePromise}>
+        <CheckoutForm />
+    </Elements>
+);
+
+export default PaymentGateway;  
