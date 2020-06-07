775830e6e483d25ebc57a7f0a0425e80573207f7
Manank Patni <manank321@gmail.com>
2020-06-07 15:55:43 +0530

    Fix urls naming

--- backend/downtownapi/main/urls.py
+++ backend/downtownapi/main/urls.py
@@ -5,11 +5,11 @@ urlpatterns = [
     path('', RootView.as_view(), name='root-view'),
 
     path('users/', UserList.as_view(), name='user-list'),
-    path('user/<int:pk>/', UserListDetail.as_view(), name='user-detail'),
+    path('users/<int:pk>/', UserListDetail.as_view(), name='user-detail'),
 
     path('business/', BusinessList.as_view(), name='business-list'),
     path('business/<int:pk>/', BusinessListDetail.as_view(), name='business-detail'),
 
     path('donations/', DonationList.as_view(), name='donation-list'),
-    path('donation/<int:pk>/', DonationListDetail.as_view(), name='donation-detail'),
+    path('donations/<int:pk>/', DonationListDetail.as_view(), name='donation-detail'),
 ]
