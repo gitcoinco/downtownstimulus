38ef9158710c065c75b081a5a693d814d8a29800
Sanchay Mittal <sanchaymittal@gmail.com>
2020-06-15 22:51:14 +0530

    new: images of business

--- backend/downtownapi/downtownapi/settings.py
+++ backend/downtownapi/downtownapi/settings.py
@@ -134,6 +134,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "business"),
+]
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'main.User'
