8529d49bad1c4e1a88905aa97727fe0b403b22af
Manank Patni <manank321@gmail.com>
2020-06-19 02:15:50 +0530

    Add Production NGINX Server

--- backend/downtownapi/downtownapi/settings.py
+++ backend/downtownapi/downtownapi/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'hlp5)_$^uyul5aqmd!yngg)jlf$j-#utopl3avrwrtuj4+=mhl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*', 'localhost', '127.0.0.1', '*']
 
