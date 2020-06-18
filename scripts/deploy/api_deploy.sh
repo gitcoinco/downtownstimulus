93dc168102a954449bbaaf0bdd5f6ebb20ace150
Manank Patni <manank321@gmail.com>
2020-06-19 05:01:07 +0530

    Improvements to Deployment Infra

--- scripts/deploy/api_deploy.sh
+++ scripts/deploy/api_deploy.sh
@@ -1,4 +1,5 @@
+ls
 python /backend/downtownapi/manage.py makemigrations
 python /backend/downtownapi/manage.py migrate
-python /backend/downtownapi/manage.py collectstatic
-python gunicorn /backend/downtownapi/downtownapi.wsgi
+python /backend/downtownapi/manage.py collectstatic --noinput
+gunicorn downtownapi.wsgi:application --bind 0.0.0.0:8000
