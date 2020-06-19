5aa15be15dceb8fa5d94061dd540db42a114ca4a
Manank Patni <manank321@gmail.com>
2020-06-20 02:58:23 +0530

    Add SSL Certificates

--- frontend/src/services/WebService.ts
+++ frontend/src/services/WebService.ts
@@ -4,7 +4,7 @@ import { IBusiness } from "../models/Business";
 import { IUser } from "../models/User";
 import { IDonation } from "../models/Donations";
 
-const ROOT_URL = "http://api.downtownstimulus.com";
+const ROOT_URL = "https://api.downtownstimulus.com";
 
 export const fetchAllBusinesses = (): Observable<IBusiness[]> => {
   return defer(() => {
