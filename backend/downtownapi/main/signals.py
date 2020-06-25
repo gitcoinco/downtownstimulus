65373af726c52ee0bad6edea9e6bd4939c28c7e1
Manank Patni <manank321@gmail.com>
2020-06-25 23:02:43 +0530

    Add Email Body

--- backend/downtownapi/main/signals.py
+++ backend/downtownapi/main/signals.py
@@ -10,26 +10,43 @@ from .models import User, Donation, Business
 
 @receiver(post_save, sender=User)
 def send_register_mail(sender, instance, **kwargs):
+    print(instance.oauth_uuid)
     if kwargs['created']:
-        print('Hello User ! Your Account Created')
-        current_site = '127.0.0.1:8000'
-        uid = urlsafe_base64_encode(force_bytes(instance.pk))
-        token = account_activation_token.make_token(instance)
-        params = {
-            'user_email': instance.email,
-            'user_name': instance.first_name,
-            'domain': current_site,
-            'uid': uid,
-            'token': token,
-            'user': instance,
-        }
-        subject = "Thanks for Creating an Account on Downtown Stimulus"
+        if instance.oauth_uuid:
+            print('Hello User ! Your Account Created')
+            params = {
+                'user_name': instance.first_name,
+            }
+            subject = "Thanks for Creating an Account on Downtown Stimulus"
 
-        try:
-            mail_body = get_mail_body('create_account', params)
-            send_mail(instance.email, subject, mail_body)
-        except Exception as e:
-            print(e)
+            try:
+                mail_body = get_mail_body('create_account', params)
+                print(mail_body)
+                send_mail(instance.email, subject, mail_body)
+            except Exception as e:
+                print(e)
+        else:
+            print('Hello User ! Your Account Created')
+            current_site = 'https://downtownstimulus.com'
+            uid = urlsafe_base64_encode(force_bytes(instance.pk))
+            token = account_activation_token.make_token(instance)
+            params = {
+                'user_email': instance.email,
+                'user_name': instance.first_name,
+                'domain': current_site,
+                'uid': uid,
+                'token': token,
+                'user': instance,
+            }
+            subject = "Thanks for Creating an Account on Downtown Stimulus"
+
+            try:
+                mail_body = get_mail_body('create_account', params)
+                print(mail_body)
+                send_mail(instance.email, subject, mail_body)
+            except Exception as e:
+                print(e)
+
 
 
 @receiver(post_save, sender=Donation)
@@ -48,6 +65,7 @@ def send_donation_mail(sender, instance, **kwargs):
 
         try:
             mail_body = get_mail_body('new_donation', params)
+            print(mail_body)
             send_mail(params['user_email'], subject, mail_body)
         except Exception as e:
             print(e)
