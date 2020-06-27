7e92cacbb7d8ac326a2f14bed92c5ded5c51df8f
Manank Patni <manank321@gmail.com>
2020-06-27 18:32:34 +0530

    Fix Email Body and Signals

--- backend/downtownapi/main/signals.py
+++ backend/downtownapi/main/signals.py
@@ -69,22 +69,3 @@ def send_donation_mail(sender, instance, **kwargs):
             send_mail(params['user_email'], subject, mail_body)
         except Exception as e:
             print(e)
-
-
-@receiver(post_save, sender=Business)
-def send_business_mail(sender, instance, **kwargs):
-    if kwargs['created']:
-        print('Hello User ! Your Business Listed Created')
-
-        params = {
-            'owner_email': instance.owner_email,
-            'business_name': instance.name
-        }
-
-        subject = "Thanks for Listing Your Business on Downtown Stimulus"
-
-        try:
-            mail_body = get_mail_body('new_business', params)
-            send_mail(params['owner_email'], subject, mail_body)
-        except Exception as e:
-            print(e)
