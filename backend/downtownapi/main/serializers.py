6ee42573a1dff1674e2ab8a5c7c849711fd6d831
Manank Patni <manank321@gmail.com>
2020-07-01 01:55:01 +0530

    Remove Measurement Id from Firebase Config

--- backend/downtownapi/main/serializers.py
+++ backend/downtownapi/main/serializers.py
@@ -19,7 +19,6 @@ FIREBASE_PROJECT_ID = os.environ.get('FIREBASE_PROJECT_ID')
 FIREBASE_STORAGE_BUCKET = os.environ.get('FIREBASE_STORAGE_BUCKET')
 FIREBASE_MESSAGING_SENDER_ID = os.environ.get('FIREBASE_MESSAGING_SENDER_ID')
 FIREBASE_APPID = os.environ.get('FIREBASE_APPID')
-FIREBASE_MEASUREMENTID = os.environ.get('FIREBASE_MEASUREMENTID')
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -138,8 +137,7 @@ class LoginTokenSerializer(serializers.Serializer):
                     'projectId': FIREBASE_PROJECT_ID,
                     'storageBucket': FIREBASE_STORAGE_BUCKET,
                     'messagingSenderId': FIREBASE_MESSAGING_SENDER_ID,
-                    'appId': FIREBASE_APPID,
-                    'measurementId': FIREBASE_MEASUREMENTID
+                    'appId': FIREBASE_APPID
                 })
             try:
                 user = User.objects.get(username=username)
